dba2f9f6f2abcf6327b420da02636f6bedff6bdb
khalilbenyahyaten <2300@holbertonschool.com>
2022-01-05 15:04:00 +0100

    safe first element

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # The types of the elements of the input are not know
-"""type"""
+"""typing"""
 from typing import Sequence, Union, Any
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """ Any """
+    """Type Any"""
     if lst:
         return lst[0]
     else:
