09d18af6314cb145c87a5a3a197a07f45854c742
khalilbenyahyaten <2300@holbertonschool.com>
2022-01-28 13:21:19 +0100

    valid email

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- valid email
+-- valid email
+DELIMITER $$
+DROP TRIGGER IF EXISTS reset_valid_email;
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END$$
