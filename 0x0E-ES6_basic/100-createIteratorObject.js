fa0d8a27a31c8ebadf1b064321fdfbce861d4987
khalilbenyahyaten <2300@holbertonschool.com>
2022-01-30 04:54:17 +0100

    create iterator object

---
+++ 0x0E-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,7 @@
+export default function* createIteratorObject(report) {
+    for (const department of Object.keys(report.allEmployees)) {
+      for (const employee of report.allEmployees[department]) {
+        yield employee;
+      }
+    }
+  }
